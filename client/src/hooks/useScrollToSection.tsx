c81b0430b4e665d658753a54014f765600940e06
Yunu <gyunu47@gmail.com>
2023-04-19 00:10:43 +0900

    feat: 메뉴 클릭시 스크롤

---
+++ client/src/hooks/useScrollToSection.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export default function useScrollToSection() {
+  const { pathname, hash } = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      const element = document.getElementById(hash.slice(1));
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    } else {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }
+  }, [pathname, hash]);
+
+  return null;
+}
--- client/src/routes/root/index.tsx
+++ client/src/routes/root/index.tsx
@@ -1,7 +1,31 @@
+import { Link, Outlet } from 'react-router-dom';
+import './index.scss';
+import useScrollToSection from '../../hooks/useScrollToSection';
+
 export default function Root() {
+  useScrollToSection();
+
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <div className="root-container">
+      <header className="header">
+        <div className="header__logo">GitCloud</div>
+        <nav className="header__navbar navbar">
+          <ul className="navbar__list list">
+            <li className="list__item">
+              <Link to="/#overview">개요</Link>
+            </li>
+            <li className="list__item">
+              <Link to="/#how-to-use">사용방법</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="header__login-wrapper login">
+          <button className="login__github">Github</button>
+        </div>
+      </header>
+      <section id="overview" className="root-section"></section>
+      <hr />
+      <section id="how-to-use" className="root-section"></section>
+    </div>
   );
 }
